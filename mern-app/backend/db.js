c4c4d23b001e8e1fe7fa2c8dead283f00fa6250c
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-07 00:21:26 +0530

    Connect with mongoose collection

--- mern-app/backend/db.js
+++ mern-app/backend/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Foodiezy:foodiezymernapp@cluster0.j1w2d4e.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://Foodiezy:foodiezymernapp@cluster0.j1w2d4e.mongodb.net/foodiezymern?retryWrites=true&w=majority';
 
 const mongoDB = async () => {
   mongoose.set("strictQuery", false);
-  await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+  await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
     if (err) console.log("---", err)
     else {
       console.log("Connected Successfully")
+      const fetched_data = await mongoose.connection.db.collection("food_items")
+      fetched_data.find({}).toArray(function (err, data) {
+        if (err) console.log(err);
+        else console.log();
+      })
     }
   });
 }
