6556950b74a040002c87e32c3c09832cdafc5582
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-11 00:18:59 +0530

    Add Img Src and comment description

--- mern-app/src/components/Card.js
+++ mern-app/src/components/Card.js
@@ -9,10 +9,10 @@ export default function Card(props) {
     <div>
       <div>
         <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-          <img className="card-img-top" src="https://images.unsplash.com/photo-1574085733277-851d9d856a3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FrZXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60" alt="Card image cap" />
+          <img className="card-img-top" src={props.imgSrc} alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title text-center">{props.foodName}</h5>
-            <p className="card-text text-center fs-6 fst-italic fw-lighter">{props.desc}</p>
+            {/* <p className="card-text text-center fs-6 fst-italic fw-lighter">{props.desc}</p> */}
             <div className='container w-100'>
               <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
                 {Array.from(Array(6), (e, i) => {
