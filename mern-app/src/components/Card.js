2cb796f34d509e180e4c35af745c85331c5cee64
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-11 12:29:55 +0530

    set styles for img

--- mern-app/src/components/Card.js
+++ mern-app/src/components/Card.js
@@ -7,27 +7,25 @@ export default function Card(props) {
 
   return (
     <div>
-      <div>
-        <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-          <img className="card-img-top" src={props.imgSrc} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title text-center">{props.foodName}</h5>
-            {/* <p className="card-text text-center fs-6 fst-italic fw-lighter">{props.desc}</p> */}
-            <div className='container w-100'>
-              <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
-                {Array.from(Array(6), (e, i) => {
-                  return (
-                    <option key={i + 1} value={i + 1}> {i + 1} </option>
-                  )
-                })}
-              </select>
-              <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
-                {priceOptions.map((data) => {
-                  return <option key={data} value={data}>{data}</option>
-                })}
-              </select>
-              <div className='d-inline h-100 fs-5'>Total Price</div>
-            </div>
+      <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
+        <img className="card-img-top" src={props.imgSrc} alt="Card image cap" style={{ height: "200px", objectFit: "fill" }} />
+        <div className="card-body">
+          <h5 className="card-title text-center">{props.foodName}</h5>
+          {/* <p className="card-text text-center fs-6 fst-italic fw-lighter">{props.desc}</p> */}
+          <div className='container w-100'>
+            <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}> {i + 1} </option>
+                )
+              })}
+            </select>
+            <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
+              {priceOptions.map((data) => {
+                return <option key={data} value={data}>{data}</option>
+              })}
+            </select>
+            <div className='d-inline h-100 fs-5'>Total Price</div>
           </div>
         </div>
       </div>
