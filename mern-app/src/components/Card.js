4bc65fdd869bf700f40d6a7bfe66fe1288e858f9
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-12 12:50:38 +0530

    Add functionality for options ,set Price ,set qty ,set size

--- mern-app/src/components/Card.js
+++ mern-app/src/components/Card.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatchCart, useCart } from './ContextReducer'
 
 export default function Card(props) {
-
+  let dispatch = useDispatchCart()
+  let data = useCart()
+  const priceRef = useRef()
   let options = props.options
   let priceOptions = Object.keys(options)
+  const [qty, setQty] = useState(1)
+  const [size, setSize] = useState("")
+  // let foodItem = props.foodItems;
+  const handleAddToCart = async () => {
+    await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+    console.log(data)
+  }
 
+  let finalPrice = qty * parseInt(options[size])
+  useEffect(() => {
+    setSize(priceRef.current.value)
+  }, [])
   return (
     <div>
       <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-        <img className="card-img-top" src={props.imgSrc} alt="Card image cap" style={{ height: "200px", objectFit: "fill" }} />
+        <img className="card-img-top" src={props.foodItem.img} alt="Card image cap" style={{ height: "100px", objectFit: "cover" }} />
         <div className="card-body">
-          <h5 className="card-title text-center">{props.foodName}</h5>
+          <h5 className="card-title text-center">{props.foodItem.name}</h5>
           {/* <p className="card-text text-center fs-6 fst-italic fw-lighter">{props.desc}</p> */}
           <div className='container w-100'>
-            <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
+            <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }} onChange={(e) => setQty(e.target.value)}>
               {Array.from(Array(6), (e, i) => {
                 return (
                   <option key={i + 1} value={i + 1}> {i + 1} </option>
                 )
               })}
             </select>
-            <select className="m-2 h-100 bg-success rounded" style={{ "color": "#fff" }}>
+            <select className="m-2 h-100 bg-success rounded" ref={priceRef} style={{ "color": "#fff" }} onChange={(e) => setSize(e.target.value)}>
               {priceOptions.map((data) => {
                 return <option key={data} value={data}>{data}</option>
               })}
             </select>
-            <div className='d-inline h-100 fs-5'>Total Price</div>
+            <div className='d-inline h-100 fs-5'>
+              ₹{finalPrice}/-
+            </div>
           </div>
+          <hr />
+          <button className='btn btn-success justify-center ms-2' onClick={handleAddToCart}>Add to Cart</ button>
         </div>
       </div>
     </div>
