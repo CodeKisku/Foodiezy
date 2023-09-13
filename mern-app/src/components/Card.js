0b612de237d48aac0746a5c47a0b60a12b23265f
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-13 23:14:54 +0530

    Add conditions for update food item if qty is modified and option is same, if option is changed then it will add new food item

--- mern-app/src/components/Card.js
+++ mern-app/src/components/Card.js
@@ -11,8 +11,27 @@ export default function Card(props) {
   const [size, setSize] = useState("")
   // let foodItem = props.foodItems;
   const handleAddToCart = async () => {
+    let food = []
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item
+
+        break;
+      }
+    }
+    if (food !== []) {
+      if (food.size === size) {
+        await dispatch({ type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty })
+        return
+      } else if (food.size !== size) {
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+        return
+        // console.log(data)
+      }
+      return
+    }
     await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
-    console.log(data)
+
   }
 
   let finalPrice = qty * parseInt(options[size])
