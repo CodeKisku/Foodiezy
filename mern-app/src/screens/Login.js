45fb7618d8f71e13659b36e2186272c996e36a29
Sanatan Kisku <sanatankisku@gmail.com>
2023-09-14 13:10:38 +0530

    Store user email to local storage

--- mern-app/src/screens/Login.js
+++ mern-app/src/screens/Login.js
@@ -25,6 +25,7 @@ export default function Login() {
     }
 
     if (json.success) {
+      localStorage.setItem("userEmail", credentials.email)
       localStorage.setItem("authToken", json.authToken)
       navigate("/")
     }
